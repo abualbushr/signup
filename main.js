5c18fefb9b48e9541b021f70fb4de2fefde8178a
Abualbushr Ibrahim Hussein Mohamed <afabunn9@gmail.com>
2024-03-02 11:57:44 +0530

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+let color = 'blue';
+switch (color) {
+    case 'blue':
+        console.log('the color is blue');
+        break;
+    case 'red':
+        console.log('the color is red');
+        break;
+
+    default:
+        console.log('the coloor dosnt match any');
+        break;
+}
